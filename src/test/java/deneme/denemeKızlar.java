340daa298c61e03b96dffbd6a61dbc55acf55f62
SumeyraGnl <h.berengunel@gmail.com>
2023-05-24 11:11:51 +0300

    sumeyra 14.05/v.1

--- src/test/java/deneme/denemeKızlar.java
+++ src/test/java/deneme/denemeKızlar.java
@@ -2,6 +2,9 @@ package deneme;
 
 public class denemeKızlar {
     public static void main(String[] args) {
+
         System.out.println("bütün kızlar toplandık");
+        System.out.println("sumeyra ekleme yaptı");
+
     }
 }
