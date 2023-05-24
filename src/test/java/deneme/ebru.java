35cc661626874e3ea6d9974cd07c78a359dd509c
EBRU <ebrusavas.2015@gmail.com>
2023-05-24 10:28:56 +0300

    first commit

---
+++ src/test/java/deneme/ebru.java
@@ -0,0 +1,18 @@
+package deneme;
+
+public class ebru {
+    public static void main(String[] args) {
+        String isim= "Ebru";
+
+        /*
+        git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ebru2535/projeDeneme1.git
+git push -u origin main
+         */
+
+
+    }
+}
